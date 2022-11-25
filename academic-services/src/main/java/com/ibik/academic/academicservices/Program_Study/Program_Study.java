f0f59cb742ffc1071eaafabe74c1bcf02c297911
NicholasFico <92150095+NicholasFico@users.noreply.github.com>
2022-11-25 12:34:45 +0700

    Tugas_6

--- academic-services/src/main/java/com/ibik/academic/academicservices/Program_Study/Program_Study.java
+++ academic-services/src/main/java/com/ibik/academic/academicservices/Program_Study/Program_Study.java
@@ -3,69 +3,75 @@ package com.ibik.academic.academicservices.Program_Study;
 import java.io.Serializable;
 import java.util.Set;
 
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
 import com.ibik.academic.academicservices.program.Programs;
 
 @Entity
-@Table(name="program_study")
+@Table(name = "Program_Study")
 public class Program_Study implements Serializable{
-
+    
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    
+
     @Column(length = 50)
     @NotEmpty(message = "Name is required")
     private String name;
 
+    @Column(length = 20)
     private String description;
 
-    @Column(length = 10, unique = true)
+    @Column(length = 5)
     @NotEmpty(message = "Code is required")
     private String code;
 
-    //@NotEmpty(message = "Program id is required")
+    // @Column(length = 11)
+    // private int program_id;
     @ManyToOne
     @JoinColumn(name = "program_id")
     private Programs programs;
 
-    //@NotEmpty(message = "Faculty is required")
+    // @Column(length = 11)
+    // private int faculty_id;
     @OneToMany
     @JoinColumn(name="faculty_id")
     private Set<Program_Study> Departments;
-    //private int faculty_id;
 
-    // @OneToMany
-    // @JoinColumn(name="department_id")
-    // private Set<ProgramStudy> departments;
-    //private int department_id;
+    @Column(length = 11)
+    private int department_id;
 
-    @Column(columnDefinition = "TINYINT(1)")
+    @Column(nullable = false, columnDefinition = "TINYINT(1)")
     private boolean is_active;
 
     public Program_Study() {
     }
 
-    public Program_Study(int id, @NotEmpty(message = "Name is required") String name, String description,
-            @NotEmpty(message = "Code is required") String code, int program_id, int faculty_id, int department_id,
-            boolean is_active) {
+    public Program_Study(int id, String name, String description, String code, Programs programs, Set<Program_Study> Departments,
+            int department_id, boolean is_active) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.code = code;
+        this.programs = programs;
+        this.Departments = Departments;
+        this.department_id = department_id;
         this.is_active = is_active;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
     }
 
     public int getId() {
@@ -100,20 +106,20 @@ public class Program_Study implements Serializable{
         this.code = code;
     }
 
-    public Programs getPrograms() {
-        return programs;
-    }
-
-    public void setPrograms(Programs programs) {
-        this.programs = programs;
-    }
-
     public Set<Program_Study> getDepartments() {
         return Departments;
     }
 
     public void setDepartments(Set<Program_Study> departments) {
         Departments = departments;
+    }
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
     }
 
     public boolean isIs_active() {
@@ -124,4 +130,11 @@ public class Program_Study implements Serializable{
         this.is_active = is_active;
     }
 
+    public Programs getPrograms() {
+        return programs;
+    }
+
+    public void setPrograms(Programs programs) {
+        this.programs = programs;
+    }
 }
--- academic-services/src/main/java/com/ibik/academic/academicservices/Students/Students.java
+++ academic-services/src/main/java/com/ibik/academic/academicservices/Students/Students.java
@@ -8,11 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinTable;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
@@ -21,135 +20,157 @@ import com.ibik.academic.academicservices.Program_Study.Program_Study;
 import com.ibik.academic.academicservices.program.Programs;
 
 @Entity
-@Table(name="students")
-public class Students implements Serializable {
+@Table(name = "Students")
+public class Students implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
 
-  private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private int id;
+    @Column(length = 15)
+    @NotEmpty(message = "NPM is required")
+    private String npm;
 
-  @Column(length = 15)
-  @NotEmpty(message = "NPM is required")
-  private String npm;
+    @Column(length = 10)
+    @NotEmpty(message = "Firstname is required")
+    private String firstname;
 
-  @Column(length = 10)
-  @NotEmpty(message = "Firstname is required")
-  private String firstname;
+    @Column(length = 10)
+    private String middlename;
 
-  @Column(length = 10)
-  private String middlename;
+    @NotEmpty(message = "Lastname is required")
+    private String lastname;
 
-  @Column(length = 10)
-  @NotEmpty(message = "Lastname is required")
-  private String lastname;
+    @Column
+    @NotEmpty(message = "Email is required")
+    private String email;
 
-  @ManyToMany
+    @Column(columnDefinition = "DATE")
+    @NotEmpty(message = "Birthdate is required")
+    private String birthdate;
+
+    //@Min(value = 1, message = "Program is required")
+    // private int program_id;
+    @ManyToOne
+    @JoinColumn(name = "program_id")
+    private Programs programs;
+
+    // @Min(value = 1, message = "Department is required")
+    // private int department_id;
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Program_Study programStudy;
+
+    @ManyToMany
     @JoinTable(
         name = "student_rel_courses",
         joinColumns = @JoinColumn(name = "student_id"),
-        inverseJoinColumns = @JoinColumn(name = "course_id")
+        inverseJoinColumns = @JoinColumn(name="course_id")
     )
-  private Set<Courses> courses;
+    private Set<Courses> courses;
 
-  @ManyToOne
-  @JoinColumn(name = "program_id")
-  private Programs program_id;
+    public Students() {
+    }
 
-  @OneToMany
-  @JoinColumn(name = "program_study_id")
-  private Set<Program_Study> program_study;
-
-  // @Min(value = 1, message = "Program is required")
-  // private int program_id;
-
-  // @Min(value = 1, message = "Departement is required")
-  // private int departement_id;
-
-  public Students() {
-  }
-
-  public Students(int id, @NotEmpty(message = "NPM is required") String npm,
-            @NotEmpty(message = "Firstname is required") String firstname, String middlename,
-            @NotEmpty(message = "Lastname is required") String lastname,
-            @NotEmpty(message = "Program is required") int program_id,
-            @NotEmpty(message = "Department is required") int department_id) {
+    public Students(int id, String npm, String firstname, String middlename, String lastname, Programs programs,
+        Program_Study programStudy, Set<Courses> courses, String email, String birthdate) {
         this.id = id;
         this.npm = npm;
         this.firstname = firstname;
         this.middlename = middlename;
         this.lastname = lastname;
+        this.programs = programs;
+        this.programStudy = programStudy;
+        this.courses = courses;
+        this.email = email;
+        this.birthdate = birthdate;
     }
 
-  public static long getSerialversionuid() {
-    return serialVersionUID;
-  }
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
 
-  public int getId() {
-    return id;
-  }
+    public int getId() {
+        return id;
+    }
 
-  public void setId(int id) {
-    this.id = id;
-  }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-  public String getNpm() {
-    return npm;
-  }
+    public String getNpm() {
+        return npm;
+    }
 
-  public void setNpm(String npm) {
-    this.npm = npm;
-  }
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
 
-  public String getFirstname() {
-    return firstname;
-  }
+    public String getFirstname() {
+        return firstname;
+    }
 
-  public void setFirstname(String firstname) {
-    this.firstname = firstname;
-  }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
-  public String getMiddlename() {
-    return middlename;
-  }
+    public String getMiddlename() {
+        return middlename;
+    }
 
-  public void setMiddlename(String middlename) {
-    this.middlename = middlename;
-  }
+    public void setMiddlename(String middlename) {
+        this.middlename = middlename;
+    }
 
-  public String getLastname() {
-    return lastname;
-  }
+    public String getLastname() {
+        return lastname;
+    }
 
-  public void setLastname(String lastname) {
-    this.lastname = lastname;
-  }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
-  public Set<Courses> getCourses() {
-    return courses;
-  }
+    public Programs getPrograms() {
+        return programs;
+    }
 
-  public void setCourses(Set<Courses> courses) {
-    this.courses = courses;
-  }
+    public void setPrograms(Programs programs) {
+        this.programs = programs;
+    }
 
-  public Programs getProgram_id() {
-    return program_id;
-  }
+    public Program_Study getProgramStudy() {
+        return programStudy;
+    }
 
-  public void setProgram_id(Programs program_id) {
-    this.program_id = program_id;
-  }
+    public void setProgramStudy(Program_Study programStudy) {
+        this.programStudy = programStudy;
+    }
 
-  public Set<Program_Study> getProgram_study() {
-    return program_study;
-  }
+    public Set<Courses> getCourses() {
+        return courses;
+    }
 
-  public void setProgram_study(Set<Program_Study> program_study) {
-    this.program_study = program_study;
-  }
+    public void setCourses(Set<Courses> courses) {
+        this.courses = courses;
+    }
 
-  
-  
+    public String getEmail() {
+      return email;
+    }
+
+    public void setEmail(String email) {
+      this.email = email;
+    }
+
+    public String getBirthdate() {
+      return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+      this.birthdate = birthdate;
+    }
+
+    
 }
